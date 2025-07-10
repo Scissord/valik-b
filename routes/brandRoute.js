e89a9c5485c88dab3a876cc7edbdfd38b8bfc0fc
Scissord <scissxrd@gmail.com>
2025-07-10 14:18:10 +0500

    brands/units

--- routes/brandRoute.js
+++ routes/brandRoute.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import * as controller from '#controllers/brandController.js';
 import validate from '#middleware/validate.js';
+import { createBrandValidation } from '#validations/brand/create.js';
+import { updateBrandValidation } from '#validations/brand/update.js';
+import { deleteBrandValidation } from '#validations/brand/delete.js';
 
 const router = express.Router();
 
-router.get('', controller.get);
+router.get('',  controller.get);
+router.post('', validate(createBrandValidation), controller.create);
+router.patch('/:brand_id', validate(updateBrandValidation), controller.update);
+router.delete('/:brand_id', validate(deleteBrandValidation), controller.softDelete);
 
 export default router;
--- routes/unitRoute.js
+++ routes/unitRoute.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import * as controller from '#controllers/unitController.js';
 import validate from '#middleware/validate.js';
+import { createUnitValidation } from '#validations/unit/create.js';
+import { updateUnitValidation } from '#validations/unit/update.js';
+import { deleteUnitValidation } from '#validations/unit/delete.js';
 
 const router = express.Router();
 
 router.get('', controller.get);
+router.post('', validate(createUnitValidation), controller.create);
+router.patch('/:unit_id', validate(updateUnitValidation), controller.update);
+router.delete('/:unit_id', validate(deleteUnitValidation), controller.softDelete);
 
 export default router;
