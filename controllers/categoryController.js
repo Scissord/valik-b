29ee74fd822676dde501b7618980e4893c948810
Scissord <scissxrd@gmail.com>
2025-07-25 11:15:13 +0500

    category logic

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,6 +5,25 @@ export const get = async (req, res) => {
   res.status(200).send(categories);
 };
 
+export const create = async (req, res) => {
+  const data = req.body;
+  const category = await Category.create(data);
+  res.status(200).send(category);
+};
+
+export const update = async (req, res) => {
+  const data = req.body;
+  const { category_id } = req.params;
+  const category = await Category.update(category_id, data);
+  res.status(200).send(category);
+};
+
+export const softDelete = async (req, res) => {
+  const { category_id } = req.params;
+  const category = await Category.softDelete(category_id);
+  res.status(200).send(category);
+};
+
 export const getTree = async (req, res) => {
   const categories = await Category.getTree();
   res.status(200).send(categories);
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,11 +2,21 @@ import express from 'express';
 import * as controller from '#controllers/categoryController.js';
 import validate from '#middleware/validate.js';
 import { findValidation } from '#validations/category/find.js';
+import verify from '#middleware/verify.js';
 
 const router = express.Router();
 
 // получить список всех категорий для админки по идее стоит сделать verify('supplier') но пока поху
 router.get('', controller.get);
+
+// создать категорию
+router.post('', verify('supplier'), controller.create);
+
+// обновить категорию
+router.patch('/:category_id', verify('supplier'), controller.update);
+
+// удалить категорию
+router.delete('/:category_id', verify('supplier'), controller.softDelete);
 
 // получить в виде дерева для фронта любой чел
 router.get('/tree', controller.getTree);
